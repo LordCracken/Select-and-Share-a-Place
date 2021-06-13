ae6def1e129678b814ed739e456241dde49c6f77
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-06-14 02:43:22 +0300

    add google API key as constant

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyAU6naO8H2vdpuJJljWfampJx3uhjP3mO0';
+
 const searchAddressHandler = (event: Event) => {
   event.preventDefault();
   const enteredAddress = addressInput.value;
